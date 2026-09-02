e7e61c78a221339c00ede0c9561553f3a5f3976c
saiyam <saiyamm.work@gmail.com>
2026-09-02 11:34:01 +0530

    feat(jugad): remove submit form, show only community jugads list

--- app/jugad/JugadClient.tsx
+++ app/jugad/JugadClient.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import { useState } from "react";
-import { JugadSubmitForm } from "@/components/jugad-submit-form";
 import { JugadList } from "@/components/jugad-list";
 
 export default function JugadClient() {
-  const [refresh, setRefresh] = useState(0);
+  const [refresh] = useState(0);
   return (
     <>
-      <JugadSubmitForm onCreated={() => setRefresh((n) => n + 1)} />
       <JugadList refreshSignal={refresh} />
     </>
   );
